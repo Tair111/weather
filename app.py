24b39c6434cd4083a7a6dc5decb9dac2701d3360
Alex <obra-aleksandr@yandex.ru>
2024-07-21 11:44:31 +0300

    release v2

--- app.py
+++ app.py
@@ -1,13 +1,57 @@
 from pprint import pprint
 
 import requests
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 import sqlite3
 import json
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, EqualTo
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'your_secret_key'  # Секретный ключ для защиты форм
+
 API = '5dd749b2f141ddbfd300a8b434132de1'
+
+# Пример простой базы данных пользователей (для демонстрационных целей)
+users = {
+    'admin': {
+        'username': 'admin',
+        'password': 'password'
+    },
+    'user': {
+        'username': 'user',
+        'password': '12345'
+    }
+}
+
+# Форма для входа
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+# Главная страница с формой входа
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        if username in users and users[username]['password'] == password:
+            flash('Login successful!', 'success')
+            return redirect(url_for('weather'))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
+
+    return render_template('login.html', form=form)
+
+# Защищённая страница, доступная после авторизации
+@app.route('/dashboard')
+def dashboard():
+    return 'Welcome to the dashboard!'
 
 
 @app.before_request
@@ -61,7 +105,7 @@ def weather():
     except Exception as exc:
         print(exc)
 
-    return render_template('weather.html', tempriche=tempriche, cities=cities)
+    return render_template('weather.html', tempriche=tempriche, cities=cities, users=users)
 
 def init_db():
     conn = get_db_connection()
